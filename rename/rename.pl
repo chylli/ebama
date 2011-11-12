51c3929521517acb79e9a1dafbb727a99624bf6c
chylli <lchangying@gmail.com>
2011-11-12 23:37:24 +0800

    change rename.pl

--- rename/rename.pl
+++ rename/rename.pl
@@ -4,27 +4,98 @@ use warnings;
 use feature 'say';
 
 use File::Copy;
+use PDF::API2;
 
-my $basedir = "test";
+my $basedir = "pdf";
+my %processed;
+
+mkdir $basedir;
+
 
 open my $fh, "<name.txt";
+
 while (my $rec = <$fh>) {
     $rec =~ /name: (\S+) \| gradeName: (\S+) \| Item # (\S+)/;
-    my ($name, $gradeName, $number) = ($1,$2,$3);
-    $name =~ s/-/ /g;
+    my %info;
+    
+    @info{qw(name gradeName number)} = ($1,$2,$3);
+    $info{name} =~ s/-/ /g;
 
-    my $oldname = "$number.pdf";
-    my $newdir = "$basedir/$gradeName";
-    my $newname = "$newdir/$name.pdf";
-    unless (-e $gradeName) {
-        mkdir($newdir);
-        
-    }
+    my $baseNumber;
+    $info{number} =~ /(.*)_e/;
+    $baseNumber = $1;
+    $info{baseNumber} = $baseNumber;
 
-    if (-e "$number.pdf") {
-        copy($oldname,$newname);
-        
+    if (exists $processed{$baseNumber}) {
+        copyBook(\%info);
     }
-    #say "name: ($name), gradeName: ($gradeName), No: ($number)";
+    else {
+        processBook(\%info);
+    }
+    
+
+
     
 }
+
+sub copyBook{
+    say "copying";
+
+}
+
+sub processBook{
+    my $info = shift;
+    
+    say "processing $info->{baseNumber}";
+    
+
+    my $newName = newName($info);
+    my $namepattern = "$info->{baseNumber}_e*.pdf";
+    
+    my @pdffiles = glob($namepattern);
+    @pdffiles = sort {$a cmp $b} @pdffiles;
+
+    return unless @pdffiles;
+    
+
+    my $newpdf = PDF::API2->new();
+    foreach my $pdf (@pdffiles) {
+        say "merging $pdf";
+        
+        my $oldpdf = PDF::API2->open($pdf);
+        for my $pnumber (1 .. $oldpdf->pages()) {
+            $newpdf->importpage($oldpdf,$pnumber,0);
+        }
+    }
+    $newpdf->saveas($newName);
+    $processed{$info->{baseNumber}} = $newName;
+    say "$info->{baseNumber}: $newName processed" ;
+    
+}
+
+sub newName {
+    my $info = shift;
+    my $newdir = "$basedir/$info->{gradeName}";
+
+    unless (-e $newdir) {
+        mkdir $newdir;
+    }
+    my $name = $info->{name};
+    my $newName = "$newdir/$name.pdf";
+    $info->{newName} = $newName;
+    return $newName;
+}
+
+#    $name =~ s/-/ /g;
+#    my $oldName = "$number.pdf";
+#    my $newdir = "$basedir/$gradeName";
+#    my $newname = "$newdir/$name.pdf";
+#    unless (-e $gradeName) {
+#        mkdir($newdir);
+#    }
+#
+#    if (-e "$number.pdf") {
+#        copy($oldname,$newname);
+#        
+#    }
+#    #say "name: ($name), gradeName: ($gradeName), No: ($number)";
